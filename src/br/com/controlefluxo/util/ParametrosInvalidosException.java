081c85a8293a4af471f5cc48390d1610aba0738b
gsTavares <gustavo.tavares24@outlook.com>
2023-10-15 14:23:37 -0300

    feat: construtores para a exception customizada

--- src/br/com/controlefluxo/util/ParametrosInvalidosException.java
+++ src/br/com/controlefluxo/util/ParametrosInvalidosException.java
@@ -1,5 +1,11 @@
 package br.com.controlefluxo.util;
 
 public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException() {
+        super();
+    }
 
+    public ParametrosInvalidosException(String message) {
+        super(message);
+    }
 }
