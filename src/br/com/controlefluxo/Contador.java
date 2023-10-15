51b6bca895924ec6f8c7c87cf5d51f829fbd5b20
gsTavares <gustavo.tavares24@outlook.com>
2023-10-15 14:10:56 -0300

    feat: recebendo os parametros

--- src/br/com/controlefluxo/Contador.java
+++ src/br/com/controlefluxo/Contador.java
@@ -1,8 +1,25 @@
 package br.com.controlefluxo;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Contador {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner entrada = new Scanner(System.in);
+        try {
+            System.out.print("Digite o primeiro parâmetro: ");
+            int parametroUm = entrada.nextInt();
+            System.out.print("\nDigite o segundo parâmetro: ");
+            int parametroDois = entrada.nextInt();
+
+            System.out.println("Parametro 1: " + parametroUm);
+            System.out.println("Parametro 2: " + parametroDois);
+        } catch (InputMismatchException e) {
+            System.out.println("Um ou mais parâmetros foram digitados incorretamente. Tente novamente!");
+        } finally {
+            entrada.close();
+        }
+
     }
-    
+
 }
